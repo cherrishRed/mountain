e66d3eb3cf402c8c3eead72c76e177b2902edacc
Red <seryprk@gmail.com>
2022-09-14 10:42:30 +0900

    feat: Mountain 모델 타입 생성

---
+++ Mountain/Mountain/Mountain.swift
@@ -0,0 +1,17 @@
+//
+//  Mountain.swift
+//  Mountain
+//
+//  Created by RED, Hamo on 2022/09/14.
+//
+
+import Foundation
+
+struct Mountain: Hashable {
+  let title: String
+  let subTitle: String
+  let imageName: String
+  let latitude: Double
+  let longitude: Double
+  var clear: Bool
+}
