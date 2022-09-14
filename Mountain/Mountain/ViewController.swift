6ac03f9d49155a2eb429a7b598bb1ba4e148e6d4
Red <seryprk@gmail.com>
2022-09-14 12:22:03 +0900

    feat : 산의 정보에 따라 이미지를 지도에 뿌려주기

--- Mountain/Mountain/ViewController.swift
+++ Mountain/Mountain/ViewController.swift
@@ -12,14 +12,19 @@ class ViewController: UIViewController, MKMapViewDelegate {
     let mark = MapMarker(title: "",
                          subtitle: "",
                          coordinate: CLLocationCoordinate2D(latitude: 37.55769, longitude: 126.92450))
-  let service: Service = Service()
-
+  
+    let service: Service = Service()
+    lazy var currentMountain: Mountain = service.mountainsData[0]
+  
     @IBOutlet weak var mountainMap: MKMapView!
+    @IBOutlet weak var mountainImageView: UIImageView!
+    @IBOutlet weak var mountainTitle: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mountainMap.delegate = self
-        mountainMap.addAnnotation(mark)
+        setUpMapImage()
+        setData()
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
@@ -38,6 +43,24 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         annotationView?.image = UIImage(named: "mountain")?.resize(newWidth: 20, newHeight: 20)
         return annotationView
+    }
+  
+  func setUpMapImage() {
+    var marks: [MapMarker] = []
+    marks = service.mountainsData.map { (mountain: Mountain) -> MapMarker in
+      return MapMarker(title: mountain.title, subtitle: "" , coordinate: CLLocationCoordinate2D(latitude: mountain.latitude, longitude: mountain.longitude))
+    }
+    for m in marks {
+      mountainMap.addAnnotation(m)
+    }
+    
+    print("정보개수 \(service.mountainsData.count)")
+  }
+  
+    func setData() {
+      mountainImageView.image = UIImage(systemName: "swift")?.resize(newWidth: 250, newHeight: 250)
+//      mountainImageView.image = UIImage(named: currentMountain.imageName)?.resize(newWidth: 250, newHeight: 250)
+      mountainTitle.text = currentMountain.title
     }
 
 }
